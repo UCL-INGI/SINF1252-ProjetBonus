3eff11962560b7e47e1108ae5b70175dceaf9804
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-03-18 13:50:14 +0100

    Split test suites + added error messages

--- hexadecimal/strebelle/tests.c
+++ hexadecimal/strebelle/tests.c
@@ -3,29 +3,34 @@
 #include <CUnit/basic.h>
 #include "../student_code.h"
 
-void test_int_0(void) {
+// @hex_to_int:hex_0 => L'entier 0 ne donne pas 0 en hexadécimal
+void test_hex_0(void) {
 	char *hex = hex_to_int(0);
 	CU_ASSERT_STRING_EQUAL(hex, "0");
 	free(hex);
 }
 
-void test_hex_0(void) {
+// @int_to_hex:int_0 => Le nombre hexadécimal 0 ne donne pas l'entier 0
+void test_int_0(void) {
 	char *hex = "0";
 	unsigned int n = int_to_hex(hex);
 	CU_ASSERT_EQUAL(n, 0);
 }
 
+// @hex_to_int:hex_normal => L'entier 42 ne donne pas 2A en hexadécimal
 void test_hex_normal(void) {
 	char *hex = hex_to_int(42);
 	CU_ASSERT_STRING_EQUAL(hex, "2A");
 	free(hex);
 }
 
+// @int_to_hex:int_normal => Le nombre hexadécimal 2A ne donne pas l'entier 42
 void test_int_normal(void) {
 	unsigned int n = int_to_hex("2A");
 	CU_ASSERT_EQUAL(n,42);
 }
 
+// @hex_to_int:hex_big => Le plus grand entier non signé ne donne pas FFFFFFFF en hexadécimal
 void test_hex_bigvalue(void) {
 	unsigned int big = 0-1;
 	char *hex = hex_to_int(big);
@@ -33,6 +38,7 @@ void test_hex_bigvalue(void) {
 	free(hex);
 }
 
+// @int_to_hex:int_big => Le nombre hexadécimal FFFFFFFF ne donne pas le plus grand entier non signé
 void test_int_bigvalue(void) {
 	char *hex = "FFFFFFFF";
 	unsigned int n = int_to_hex(hex);
@@ -53,18 +59,31 @@ int main(int argc, char const *argv[]) {
 		return CU_get_error();
 
 	CU_pSuite pSuite = NULL;
-	pSuite = CU_add_suite("Suite de tests : hexadecimal", setup, teardown);
+	CU_pSuite pSuite2 = NULL;
+
+	pSuite = CU_add_suite("Suite de tests : hex_to_int", setup, teardown);
 	if (NULL == pSuite) {
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
 
-	if (NULL == CU_add_test(pSuite, "Int 0", test_int_0) ||
-		NULL == CU_add_test(pSuite, "Hexa 0", test_hex_0) ||
+	if (NULL == CU_add_test(pSuite, "Hexa 0", test_hex_0) ||
 		NULL == CU_add_test(pSuite, "Hexa normal", test_hex_normal) ||
-		NULL == CU_add_test(pSuite, "Int normal", test_int_normal) ||
-		NULL == CU_add_test(pSuite, "Hexa big value", test_hex_bigvalue) ||
-		NULL == CU_add_test(pSuite, "Int big value", test_int_bigvalue)
+		NULL == CU_add_test(pSuite, "Hexa big value", test_hex_bigvalue)
+	) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	pSuite2 = CU_add_suite("Suite de tests : int_to_hex", setup, teardown);
+	if (NULL == pSuite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (NULL == CU_add_test(pSuite2, "Int 0", test_int_0) ||
+		NULL == CU_add_test(pSuite2, "Int normal", test_int_normal) ||
+		NULL == CU_add_test(pSuite2, "Int big value", test_int_bigvalue)
 	) {
 		CU_cleanup_registry();
 		return CU_get_error();
