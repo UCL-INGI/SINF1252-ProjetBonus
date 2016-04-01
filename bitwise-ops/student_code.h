ff5875071f7b84c41c47ea53969283db4c4683f5
Mathieu Xhonneux <m.xhonneux@gmail.com>
2016-04-01 20:36:11 +0200

    MAJ header bitwise-ops

--- bitwise-ops/student_code.h
+++ bitwise-ops/student_code.h
@@ -1,6 +1,6 @@
-unsigned char get_bit(unsigned int x, int pos);
-unsigned int set_bit(unsigned int x, int pos, unsigned char value);
+unsigned char get_bit(unsigned int x, unsigned int pos);
+unsigned int set_bit(unsigned int x, unsigned int pos, unsigned char value);
 unsigned char get_3_leftmost_bits(unsigned int x);
 unsigned char get_4_rightmost_bits(unsigned int x);
 unsigned int unset_first_bit(unsigned int x);
-unsigned int cycle_bits(unsigned int x, int n);
+unsigned int cycle_bits(unsigned int x, unsigned int n);
