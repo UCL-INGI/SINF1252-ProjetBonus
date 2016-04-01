270a727a46c9a5b0e77b7d0a8e24c27db6e8e15e
CharlyBVo <charles-henry.bertrand@student.uclouvain.be>
2016-04-01 22:07:38 +0200

    Added files via upload

--- btree-access/student_code.h
+++ btree-access/student_code.h
@@ -1,11 +1,9 @@
 typedef struct BSTreeNode {
     int key;
-    int data;
+    int value;
 
     struct BSTreeNode *left;
     struct BSTreeNode *right;
 } Node;
-
-int *has_key(Node root, int key);
-
+int* has_key(Node root, int key);
 int compare(Node root_a, Node root_b);
